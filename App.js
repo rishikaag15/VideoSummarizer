6fe57acb8510045878d52f10869c532c98590038
rishikaag15 <95970487+rishikaag15@users.noreply.github.com>
2021-12-11 19:57:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import BackendAPI from './components/BackendApi';
+
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>YouTube Transcript Summarizer</h1>
+				<pre><div class="line"></div></pre>
+				<BackendAPI />
+			</header>
+			<footer className="footer">
+				Made by - 18102293 Rishika Agarwal and 18102297 Nimra Zafar
+			</footer> 
+		</div>
+	);
+}
+
+export default App;
